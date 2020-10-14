4b49166419fded2dde6f3942d38d0504e13e04d3
Krystian Kupiec <dev@velein.pl>
2020-10-14 17:02:45 +0200

    refactor: button API, add default button and split variants into own files

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import React from 'react'
+
+/**
+ * Basically a button reset
+ */
+const buttonStyles = css`
+    cursor: pointer;
+    background-color: transparent;
+    padding: 0;
+    border-radius: 0;
+    border: none;
+    outline: none;
+`
+
+export const buttonTransition = css`
+    transition: background-color 250ms, color 500ms, border-color 500ms;
+`
+
+export interface ButtonProps {
+    to?: string
+    onClick?: () => void
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+    return (
+        <button css={buttonStyles} {...rest}>
+            {children}
+        </button>
+    )
+}
--- src/components/Button/story.tsx
+++ src/components/Button/story.tsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import { Button, ButtonProps } from '.'
+import { Story } from '@storybook/react/types-6-0'
+import { Button, ButtonProps, ButtonGhost, ButtonDark, ButtonLight } from '.'
 
 export default {
     title: 'Components/Button',
     component: Button,
-} as Meta
-
-const Template: Story<ButtonProps> = (args) => <Button {...args} />
-
-export const Dark = Template.bind({})
-Dark.args = {
-    variant: 'dark',
-    label: 'get in touch',
 }
 
-export const Ghost = Template.bind({})
-Ghost.args = {
-    variant: 'ghost',
-    label: 'learn more',
-}
+export const Default: Story<ButtonProps> = (args) => (
+    <Button {...args}>Click me</Button>
+)
 
-export const Light = Template.bind({})
-Light.args = {
-    variant: 'light',
-    label: 'get started',
-}
+export const Ghost: Story<ButtonProps> = (args) => (
+    <ButtonGhost {...args}>Click me</ButtonGhost>
+)
+
+export const Dark: Story<ButtonProps> = (args) => (
+    <ButtonDark {...args}>Click me</ButtonDark>
+)
+
+export const Light: Story<ButtonProps> = (args) => (
+    <ButtonLight {...args}>Click me</ButtonLight>
+)
