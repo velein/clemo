eaf1c3e6459ac88b6fbda1d7f2031926b46fc41d
Krystian Kupiec <dev@velein.pl>
2020-10-17 02:13:34 +0200

    feat: add services page

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import PageLayout from '~layouts/Page'
 import { Header } from '~components/Header'
-import { CarouselIntro } from '../modules/CarouselIntro'
 import { ServicesGrid } from '~modules/ServicesGrid'
 
-export default function Index() {
+export default function Services() {
     return (
         <PageLayout>
             <Header />
-            <CarouselIntro />
-            <ServicesGrid heading="what we do?" />
+            <ServicesGrid heading="services" />
         </PageLayout>
     )
 }
